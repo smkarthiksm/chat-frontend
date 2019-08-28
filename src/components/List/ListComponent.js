be12da4381c19ed8c50b84417d3f598719414972
Karthik SM <smkarthiksmk@gmail.com>
2019-08-29 02:52:56 +0530

    Added profile image section

--- src/components/List/ListComponent.js
+++ src/components/List/ListComponent.js
@@ -8,7 +8,12 @@ const ListComponent = (props) => {
     <div className="list-container">
       {props.state.listData.map((element, index) =>
         <div className="list-element-container row" key={element.id} onClick={() => addItem(props, element, index)}>
-          <div className="col-md-8">{element.firstName} {element.lastName}</div>
+          <div className="col-md-8">
+            <div className="image-container">
+              {element.firstName ? element.firstName[0] : ''}{element.lastName ? element.lastName[0] : ''}
+            </div>
+            {element.firstName} {element.lastName}
+          </div>
           <div className="col-md-4">{element.email}</div>
         </div>
       )}
